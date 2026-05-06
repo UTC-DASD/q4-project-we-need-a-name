c12dd464c41758bb64215febdee830485d04d86f
brodydesormeaux27-spec <brody.desormeaux27@tech4maine.org>
2026-05-06 11:43:42 -0400

    pwoject

--- UnityProjects/Q4 project/Assets/Scripts/SpikeTrapController.cs
+++ UnityProjects/Q4 project/Assets/Scripts/SpikeTrapController.cs
@@ -5,12 +5,26 @@ public class SpikeTrapController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        ResetHealth();
+
+      spriteRender = GetComponent<spriteRender>();
+      GameController.OnReset += ResetHealth;
+      HealthItem.OnHealthCollect += Heal;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Enemy enemy = collision.GetComponent<Enemy>();
+        if (enemy)
+        {
+            TakeDamage (enemy.damage);
+        }
+        Trap trap = collision.GetComponent<Trap>();
+        if (trap && trap.damage > 0)
+        {
+            TakeDamage(trap.damage);
+        }
     }
 }
