8f3f149e3f0acd6ea226c0daf91da4a39af5867f
jaydenbryden27-boop <jayden.bryden27@tech4maine.org>
2026-05-21 13:35:22 -0400

    huh

--- UnityProjects/Q4 project/Assets/Scripts/Enemycontroller.cs
+++ UnityProjects/Q4 project/Assets/Scripts/Enemycontroller.cs
@@ -6,25 +6,62 @@ using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
 
+
 public class Enemycontroller : MonoBehaviour
 {
+    
     public Transform player;
-    private NavMeshAgent navMeshAgent;
+    [SerializeField] private bool isGrounded;
+     public SpriteRenderer enemySpriteRenderer;
+    public float speed = 2f;
+    public float jumpForce = 5f;
+    Rigidbody2D rb;
+    private Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-      navMeshAgent = GetComponent<NavMeshAgent>();  
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (player != null)
         {
-            //navMeshAgent.SetDestination(player.position);  
+            transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);  
+        }
+    }
+    void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(moveInput.x * speed, rb.linearVelocity.y);
+    }
+     public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+
+        if(moveInput.x < 0 )
+        {
+            enemySpriteRenderer.flipX = true;
+        }
+        else if (moveInput.x > 0)
+        {
+            enemySpriteRenderer.flipX = false;
+        }
+    }
+ private void OnCollisionEnter2D(Collision2D collision)
+    {    
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;   
         }
 
+        if (collision.gameObject.CompareTag("Walljump"))
+        {
+            Destroy(gameObject);
+        }
 
 }
 }
+
 
